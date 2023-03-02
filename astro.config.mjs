27b7b7b7f14665a9f93a054d65b1615da8288470
Nikhil Vengal <ngvengal@gmail.com>
2023-03-02 15:50:15 -0800

    Back to barebones astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
-import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://civicsoftwareinitiative.org/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
+	integrations: [mdx(), tailwind()],
 });
