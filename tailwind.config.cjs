c8860278503b3d89f66d48749867abf4e3386d1a
Nikhil Vengal <ngvengal@gmail.com>
2023-03-03 21:17:40 -0800

    Add color diff between sections and dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      "violet-purple": "#3d0080",
-      "dark-slate-blue": "#082e4a",
-      "slate-blue": "#6e99ba",
-      "light-blue": "#d6e8fa",
-      red: "#d91f2b",
+    extend: {
+      colors: {
+        "violet-purple": "#3d0080",
+        "dark-slate-blue": "#082e4a",
+        "slate-blue": "#6e99ba",
+        "light-blue": "#d6e8fa",
+        red: "#d91f2b",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
