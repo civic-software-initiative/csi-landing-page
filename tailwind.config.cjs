09f5ca29727ceaf8f2592a0e6bf42efd07ededf0
Nikhil Vengal <ngvengal@gmail.com>
2023-03-03 16:15:37 -0800

    Add stub of about partial

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,11 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      'violet-purple': '#3d0080',
-      'dark-slate-blue': '#082e4a',
-      'slate-blue': '#6e99ba',
-      'light-blue': '#d6e8fa',
-      'red': '#d91f2b'
+      "violet-purple": "#3d0080",
+      "dark-slate-blue": "#082e4a",
+      "slate-blue": "#6e99ba",
+      "light-blue": "#d6e8fa",
+      red: "#d91f2b",
     },
     extend: {},
   },
